89b6de6d64fc86fb3e5fa12f4c0d9aacf54bf86a
Muzi Phage <muzi.phage@gmail.com>
2019-03-11 10:29:34 +0200

    Switch environment to PROD

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+    firebase: {
+        apiKey: 'AIzaSyCSrGmxx5cT3aHVXzJ-3XFFXhq9fVbH0WI',
+        authDomain: 'gubhani-75bd2.firebaseapp.com',
+        databaseURL: 'https://gubhani-75bd2.firebaseio.com',
+        projectId: 'gubhani-75bd2',
+        storageBucket: 'gubhani-75bd2.appspot.com',
+        messagingSenderId: '111603856424'
+    },
 };
